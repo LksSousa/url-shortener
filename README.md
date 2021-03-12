7a9c839a2e5abb57265ebd6115e1b1afdc8780c2
Lucas Sousa <lucassousa86@hotmail.com>
2021-03-12 19:42:56 -0300

    Projeto de encurtar URL

---
+++ README.md
@@ -0,0 +1,9 @@
+# Encurtador URL (url-shortener)
+
+Projeto realizado para construção de um Encurtador de URL usando node.js e typescript.
+
+## Rodando o projeto
+
+- `npm install`
+- `npm run build`
+- `npm run dev`
---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://dbLKS:dbLKStreinando01@@url-shortener-dio.8pmue.mongodb.net/url-shortener-dio?retryWrites=true&w=majority'
+}
