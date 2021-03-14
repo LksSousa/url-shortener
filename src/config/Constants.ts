66d2b5b5271f333fb47f3ce31ec3491286a8ed40
Lucas Sousa <lucassousa86@hotmail.com>
2021-03-13 23:42:01 -0300

    necessario inserir o password e o nome do database no arquivo Constantes.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
     API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://dbLKS:dbLKStreinando01@@url-shortener-dio.8pmue.mongodb.net/url-shortener-dio?retryWrites=true&w=majority'
+    MONGO_CONNECTION: 'mongodb+srv://dbLKS:<Password>@url-shortener-dio.8pmue.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
